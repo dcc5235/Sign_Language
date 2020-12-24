da2937250eec8e46cee7e56219d9bb2083255bf7
Dany <dany@Danys-MacBook-Pro.local>
2020-12-23 21:17:11 -0600

    assigned variables to btn and source vid

---
+++ index.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("click");
+const video = document.getElementsByTagName("source");
+
+btn.onclick = function () {
+  console.log("this works. you clicked the button");
+};

58fb5025d08a25dfe2f9b1c07a38ec05391bf0ba
Dany <dany@Danys-MacBook-Pro.local>
2020-12-25 10:35:47 -0600

    comments for functionality of video/btn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const video = document.getElementsByTagName("source");
 btn.onclick = function () {
   console.log("this works. you clicked the button");
 };
+
+// when btn is clicked, it targets video
+// video src needs to change
+// video src pulled and called from an array

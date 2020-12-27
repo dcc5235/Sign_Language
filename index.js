7d83ac3622bdec4382599a6973a4351da4a86de1
Dany <dany@Danys-MacBook-Pro.local>
2020-12-26 20:15:31 -0600

    video array

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const videoArray = [
   "assets/production ID_5211957.mp4",
   "assets/production ID_5211958.mp4",
   "assets/production ID_5211959 (1).mp4",
-  "assets/production ID_5211960.mp4"
+  "assets/production ID_5211960.mp4",
+  "assets/production ID_5211962.mp4",
+  "assets/production ID_5211963.mp4",
+  "assets/production ID_5211964.mp4",
+  "assets/production ID_5211965.mp4",
+  "assets/production ID_5211966.mp4",
+  "assets/production ID_5211967.mp4",
+  "assets/production ID_5211968.mp4",
+  "assets/production ID_5211969.mp4",
+  "assets/production ID_5211970.mp4",
+  "assets/production ID_5211971.mp4",
+  "assets/production ID_5211972.mp4",
+  "assets/production ID_5211973.mp4",
+  "assets/production ID_5212084.mp4",
+  "assets/production ID_5212085.mp4",
+  "assets/production ID_5212086.mp4",
+  "assets/production ID_5212087.mp4"
 ];
 
 
